0f4bcf335170f003caad56bb69993be968552c50
carolina mesquita <carolti2013@gmail.com>
2021-01-05 23:17:44 -0300

    Finalizando o cadastro

--- crud_java_spring/src/main/java/com/crud_java_spring/DTO/UsuarioDTO.java
+++ crud_java_spring/src/main/java/com/crud_java_spring/DTO/UsuarioDTO.java
@@ -6,9 +6,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter
-@Setter 
-@Builder
+
 public class UsuarioDTO {
 
     private String email;
@@ -17,5 +15,70 @@ public class UsuarioDTO {
     private String cpf;
     private String telefone;
     private Endereco endereco;
+
+
+    public UsuarioDTO() {
+    }
+
+    public UsuarioDTO(String email, String nome, String senha, String cpf, String telefone, Endereco endereco) {
+        this.email = email;
+        this.nome = nome;
+        this.senha = senha;
+        this.cpf = cpf;
+        this.telefone = telefone;
+        this.endereco = endereco;
+    }
+
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSenha() {
+        return this.senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getCpf() {
+        return this.cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getTelefone() {
+        return this.telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public Endereco getEndereco() {
+        return this.endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+
     
 }
+
+  
--- crud_java_spring/src/main/java/com/crud_java_spring/controller/UsuarioController.java
+++ crud_java_spring/src/main/java/com/crud_java_spring/controller/UsuarioController.java
@@ -7,10 +7,11 @@ import com.crud_java_spring.model.Usuario;
 import com.crud_java_spring.service.CepService;
 import com.crud_java_spring.service.UsuarioService;
 
-import org.apache.catalina.connector.Response;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/usuarios")
+@CrossOrigin(origins = "*")
 public class UsuarioController {
 
 	
